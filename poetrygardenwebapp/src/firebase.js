b712c03ae117459d593e21dc0af26e2b940c5395
luffysolosyonko <116454436+luffysolosyonko@users.noreply.github.com>
2025-03-19 21:57:23 -0500

    Update firebase.js

--- poetrygardenwebapp/src/firebase.js
+++ poetrygardenwebapp/src/firebase.js
@@ -6,13 +6,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-  apiKey: "AIzaSyA8a1fYSagmKb41CjTSI5PuwsMthN0Om_o",
-  authDomain: "poetrygardenfirebase.firebaseapp.com",
-  projectId: "poetrygardenfirebase",
-  storageBucket: "poetrygardenfirebase.firebasestorage.app",
-  messagingSenderId: "555010384916",
-  appId: "1:555010384916:web:ef1e7c2f5e8b83e96cf301",
-  measurementId: "G-3PXXRHKZYM"
+  
 };
   
   
