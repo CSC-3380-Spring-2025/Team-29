6c3cd9d5f1ed6a7f76f3f9368bc5692af4597698
luffysolosyonko <116454436+luffysolosyonko@users.noreply.github.com>
2025-04-19 17:29:57 -0500

    Update firebase.js

--- poetrygardenwebapp/src/firebase.js
+++ poetrygardenwebapp/src/firebase.js
@@ -2,10 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getFirestore } from "firebase/firestore";
 
-  
-  
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+};
+
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
