506703bf114b44a5b2a52ae6636cf0fae1d5b162
luffysolosyonko <116454436+luffysolosyonko@users.noreply.github.com>
2025-04-19 17:18:53 -0500

    deleted api keys

--- poetrygardenwebapp/src/firebase.js
+++ poetrygardenwebapp/src/firebase.js
@@ -3,14 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-<<<<<<< HEAD
-=======
-  
->>>>>>> b712c03ae117459d593e21dc0af26e2b940c5395
-};
   
   
 // Initialize Firebase
