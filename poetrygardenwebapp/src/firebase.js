d9699f0aeaadc7e9bad6d998d68d83ee3aa0c6aa
luffysolosyonko <116454436+luffysolosyonko@users.noreply.github.com>
2025-04-23 14:08:59 -0500

    addedbetterlogin

--- poetrygardenwebapp/src/firebase.js
+++ poetrygardenwebapp/src/firebase.js
@@ -5,15 +5,7 @@ import { getFirestore, collection, addDoc, getDocs } from 'firebase/firestore';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyA8a1fYSagmKb41CjTSI5PuwsMthN0Om_o",
-    authDomain: "poetrygardenfirebase.firebaseapp.com",
-    projectId: "poetrygardenfirebase",
-    storageBucket: "poetrygardenfirebase.firebasestorage.app",
-    messagingSenderId: "555010384916",
-    appId: "1:555010384916:web:ef1e7c2f5e8b83e96cf301",
-    measurementId: "G-3PXXRHKZYM"
-  };
+
   
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
