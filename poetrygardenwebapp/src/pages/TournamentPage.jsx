693e654a0b1ac77a785fccae612fadde66c4c3b6
luffysolosyonko <116454436+luffysolosyonko@users.noreply.github.com>
2025-04-24 22:15:27 -0500

    edited tournament page

--- poetrygardenwebapp/src/pages/TournamentPage.jsx
+++ poetrygardenwebapp/src/pages/TournamentPage.jsx
@@ -1,34 +1,66 @@
+
+import Poetrygardenlogo from '../images/Poetrygardenlogo.png'; 
 import React, { useState, useEffect } from 'react';
 import { db, addGarden, fetchGardens } from '../firebase';
-import { onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { auth } from '../firebase';
+import '../styles/aboutpage.css';
+import { useNavigate } from "react-router-dom";
 
 const TournamentPage = () => {
     const [user, setUser] = useState(null);
+    const navigate = useNavigate();
+
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
           setUser(user);
         });
         return () => unsubscribe();
-  }, []);
-        return(
-    <div>
-      {/* Navigation Bar */}
+      }, []);
+      const handleLogout = async () => {
+          try {
+            await signOut(auth);
+            setUser(null); // optional: clear local user state
+            navigate("/");
+          } catch (err) {
+            console.error("Logout failed:", err);
+          }
+        };
+
+  return (
+
+ <div>
       <nav className="navbar">
-        <h2 className="navbar-title">Poetry Garden</h2>
+        <div className="navbar-logo">
+          <img src={Poetrygardenlogo} alt="Logo" className="logo" />
+        </div>
         <div className="navbar-links">
           <a href="/">Home</a>
-          <a href="/community">Community</a>
+          <a href="/communitypage">Community</a>
           <a href="/tournament">Tournament</a>
           <a href="/mygarden">My Garden</a>
           <a href="/about">About</a>
-          {user && <span className="user-email">{user.email}</span>}
+          {user && (
+  <div className="user-info">
+    <span className="user-email">{user.email}</span>
+    
+      <button
+        className="logout-button"
+        onClick={async () => {
+          await handleLogout();
+          
+        }}
+    >
+      Logout
+    </button>
+    
+  </div>
+)}
         </div>
       </nav>
-      </div>
-    
-    );
 
+   </div>
+  );
 };
 
 export default TournamentPage;
