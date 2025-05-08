3bc431d9447ba8d89708897d17ca366f875f29b8
RiversDupaquier <rdupaq4@lsu.edu>
2025-05-07 21:57:38 -0500

    other

--- poetrygardenwebapp/src/index.ts
+++ poetrygardenwebapp/src/index.ts
@@ -3,9 +3,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from '../../backup2/App'; // Import the App component
+import App from './App'; // Import the App component
 
-import reportWebVitals from '../../backup2/reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,8 +15,6 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-customReportWebVitals(console.log);
-function customReportWebVitals(log: { (...data: any[]): void; (message?: any, ...optionalParams: any[]): void; }) {
-  throw new Error('Function not implemented.');
-}
+reportWebVitals(console.log);
+
 
